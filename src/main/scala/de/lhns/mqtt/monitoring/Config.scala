a27dcbdc573e564e923c1f7f21b8502d6640074b
Pierre Kisters <pierrekisters@gmail.com>
2026-02-18 01:24:06 +0100

    const labels and values

--- src/main/scala/de/lhns/mqtt/monitoring/Config.scala
+++ src/main/scala/de/lhns/mqtt/monitoring/Config.scala
@@ -3,7 +3,7 @@ package de.lhns.mqtt.monitoring
 import cats.syntax.all.*
 import com.comcast.ip4s.{Host, SocketAddress}
 import de.lhns.mqtt.monitoring.Config.FilterConfig
-import io.circe.Codec
+import io.circe.{Codec, JsonNumber}
 
 import scala.collection.immutable.ListMap
 import scala.concurrent.duration.{Duration, FiniteDuration}
@@ -15,7 +15,7 @@ case class Config(
                    maxCardinality: Option[Int],
                    exportInterval: Option[FiniteDuration],
                    filters: List[FilterConfig]
-                 )derives Codec
+                 ) derives Codec
 
 object Config {
   case class FilterConfig(
@@ -23,20 +23,23 @@ object Config {
                            topics: List[TopicPattern],
                            discard: Option[Boolean],
                            topicLabel: Option[Boolean],
+                           labels: Option[ListMap[String, String]],
                            labelMatchers: Option[List[ExtendedGroupPattern]],
-                           valueMappings: Option[ListMap[ExtendedGroupPattern, String]],
+                           valueMappings: Option[ListMap[ExtendedGroupPattern, ExtendedGroupPattern.Replacement]],
                            enableArrays: Option[Boolean],
                            ignoreWarnings: Option[Boolean]
-                         )derives Codec {
+                         ) derives Codec {
     val metricNameOrDefault: String = metricName.getOrElse("mqtt.value")
 
     val discardOrDefault: Boolean = discard.getOrElse(false)
 
     val topicLabelOrDefault: Boolean = topicLabel.getOrElse(true)
 
+    val labelsOrDefault: ListMap[String, String] = labels.getOrElse(ListMap.empty)
+
     val labelMatchersOrDefault: List[ExtendedGroupPattern] = labelMatchers.getOrElse(List.empty)
 
-    val valueMappingsOrDefault: ListMap[ExtendedGroupPattern, String] = valueMappings.getOrElse(ListMap.empty)
+    val valueMappingsOrDefault: ListMap[ExtendedGroupPattern, ExtendedGroupPattern.Replacement] = valueMappings.getOrElse(ListMap.empty)
 
     val enableArraysOrDefault: Boolean = enableArrays.getOrElse(false)
 
@@ -49,6 +52,12 @@ object Config {
     case duration: FiniteDuration => duration
     case e => throw new RuntimeException(s"Unsupported duration: $e")
   })(_.toString)
+
+  private given Codec[BigDecimal] = Codec.implied[JsonNumber].imap(e =>
+    e.toBigDecimal.getOrElse(throw new RuntimeException(s"Invalid decimal: $e"))
+  )(e =>
+    JsonNumber.fromDecimalStringUnsafe(e.toString)
+  )
 
   def fromEnv: Config =
     sys.env.get("CONFIG")
